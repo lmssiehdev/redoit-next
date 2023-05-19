2d7ba2f15700f1294e96293cc580ceb84be8b8d5
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-05-19 19:34:04 +0100

    clean up

--- src/app/web/layout.tsx
+++
@@ -1,9 +0,0 @@
-import SupabaseProvider from "@/context/supabase-provider";
-
-export default function WebLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <SupabaseProvider>{children}</SupabaseProvider>
-    </>
-  );
-}
