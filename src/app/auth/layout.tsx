f723f77f7d7491ea5f25d36cdff0c0d9779d0dc0
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-05-19 19:38:02 +0100

    fix: typescript error

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,20 +1,8 @@
-import SupabaseProvider from "@/context/supabase-provider";
-
 export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const supabase = createClient();
-  // const {
-  //   data: { user },
-  // } = await supabase.auth.getUser();
-  // const { data } = await supabase.from("habits").select().eq("id", user);
-
   const data = {};
-  return (
-    <div>
-      <SupabaseProvider>{children}</SupabaseProvider>
-    </div>
-  );
+  return <div>{children}</div>;
 }
